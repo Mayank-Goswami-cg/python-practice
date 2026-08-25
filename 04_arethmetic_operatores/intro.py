8d9a20bf74840b6145850c5b3cf70888b7a9fd8d
Mayank-Goswami-cg <mayank.goswami.cg@gmail.com>
2026-08-25 11:30:07 +0530

    arithematic operations added

---
+++ 04_arethmetic_operatores/intro.py
@@ -0,0 +1,22 @@
+# Arethematic Operators
+
+a=-19
+b=26
+
+c=10
+d=-50
+
+print("addition",a+b)
+print("subtraction",a-b)
+print("multiplication", c*d)
+print("devision", c/d)
+print("floorDivision", c//d)
+print("Modulus", d//10)
+
+#Arethematic Operators in string Type
+
+A="Mayank"
+B=5
+
+print(A+" "+str(B))
+print(A*B)
