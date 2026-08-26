279eeefb5d154d7f0468a4f917363975adcc31eb
Mayank-Goswami-cg <mayank.goswami.cg@gmail.com>
2026-08-26 10:03:46 +0530

    Arithematic assignment file created and all work finished

---
+++ 04_arethmetic_operatores/assignment.py
@@ -0,0 +1,209 @@
+#Part-2 Predict the output
+
+
+#Question 1
+print(10 + 5)  #15
+print(10 - 5)  #5
+print(10 * 5)  #50
+print(10 / 5)  #2
+print(10 // 3) #3
+print(10 % 3)  #1
+print(2 ** 4)  #16
+
+#Question 2
+print(10 + 5 * 2)  #20
+print((10 + 5) * 2)#30
+
+#Question 3
+print(20 / 5 * 2)  #8
+print(20 // 3 * 2) #12
+print(20 - 5 + 2)  #17
+
+#Question 4
+print(10 - (-5))  #15
+print(-10 - 5)    #-15
+print(-10 - (-5)) #-5
+
+#Question 5
+print(10 // 3)   #3
+print(-10 // 3)  #-4
+print(10 // -3)  #-4
+print(-10 // -3) #3
+
+#Question 6
+print(10 % 3)  #1
+print(-10 % 3) #2
+print(10 % -3) #-2
+print(-10 % -3)#1
+
+#Question 7
+print(-2 ** 2)   #-4
+print((-2) ** 2) #4
+print(-2 ** 3)   #-8
+print((-2) ** 3) #-8
+
+#Question 8
+print(True + True)  #2
+print(True + False) #1
+print(False * 10)   #0
+print(True * 10)    #10
+print(True / 2)     #0.5
+
+#Question 9
+print("Python" + "Programming")  #PythonProgramming
+print("Python" * 3)              #PythonPythonPython
+
+
+
+#Part-3 Practicle Programs
+
+#Task 1
+a=10
+b=50
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+#Task 2
+
+c=10
+d=0.5
+print(c+d)
+print(c-d)
+print(c*d)
+print(c/d)
+print(c//d)
+print(c%d)
+print(c**d)
+
+#The Data Type of the Result is Float in All Cases
+
+
+#Task 3
+e=85
+f=90
+g=95
+
+print("Total Marks",e+f+g)
+print("Average Marks",(e+f+g)/3)
+
+
+#Task 4
+#product price="p" & Quantity="q"
+
+p=499
+q=100
+
+print(p*q)#Total Price="49,900"
+
+
+#Task 5
+
+h=95
+print("Modulus",h%2)
+print("Modulus",h%3)
+
+#Task 6
+
+i=82
+j=3
+k=-82
+l=-3
+print("Division",i/j)
+print("Floor Division",i//j)
+print("Division-",k/l)
+print("Floor Division-",k//l)
+
+#Task 7
+
+m=-3
+print("Addition",k+m)
+print("Subtraction",k-m)
+print("Division",k/m)
+print("Floor Division",k//m)
+print("Modulus",k%m)
+print("Multiplication",k*m)
+
+#Task 8
+
+print("+ - +",i-j)
+print("+ - -",i-k)
+print("- - +",k-i)
+print("- - -",k-l)
+
+#Task 9
+
+print("+//+",i//j)
+print("+//-",i//k)
+print("-//+",k//i)
+print("-//-",k//l)
+
+#Task 10
+
+print("+%+",i%j)
+print("+%-",i%k)
+print("-%+",k%i)
+print("-%-",k%l)
+
+
+#Part 4 — Operator Precedence
+
+#Task 11
+
+print(10 + 5 * 2) #Multiplication 20
+print(20 - 4 / 2) #Division 18
+print(10 + 20 / 5 * 2) #Division 18
+print(2 + 3 * 4 ** 2) #Multiplication 146
+print(100 - 20 // 5) #Floor Division 96
+
+#Task 12
+
+print(10 + 5 * 2) #20
+print((10 + 5) * 2) #30
+
+print(20 - 10 / 2) #15
+print((20 - 10) / 2) #5
+
+print(2 + 3 * 4) #14
+print((2 + 3) * 4) #20
+
+#Part 5 — Boolean Arithmetic
+
+#Task 13
+
+A=True
+B=False
+
+print("Addition",A+B)
+print("Subtraction",A-B)
+print("Division",B/A)
+print("Floor Division",B//A)
+print("Modulus",B%A)
+print("Multiplication",B*A)
+print("Exponentiation",B**A)
+
+#Task 14
+
+print(True + 5)
+print(False + 5)
+print(True * 10)
+print(False * 10)
+print(True - 5)
+print(False - 5)
+
+#Part 6 — String Operations
+
+#Task 15,16&17
+
+C="Mayank"
+D="Goswami"
+print(C+D)
+print(C*5)
+print(C+D)#string + string 
+#string - string
+print(C*A)# string * integer
+print(C/A)# string / string
