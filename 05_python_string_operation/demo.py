deee047b16c49a52881dd82eecdd410db7b15a8c
Mayank-Goswami-cg <mayank.goswami.cg@gmail.com>
2026-08-26 10:46:03 +0530

    Python string operators learned

---
+++ 05_python_string_operation/demo.py
@@ -0,0 +1,19 @@
+#Indexing
+#PYTHON
+#012345
+#-6,-5,-4,-3,-2,-1
+
+b="Python"
+print(b[5], b[-5])
+
+# == (comparision operator) output in boolean
+
+a="Python"
+print(a==b)
+
+#Line break through Triple qoutes and \n
+
+print("""Dear Sir,
+       Good Morning.""")
+
+print("Dear Ma'am, \n       Good Morning.")
