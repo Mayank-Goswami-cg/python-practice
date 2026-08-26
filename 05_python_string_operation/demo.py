67c77526cf529851f0bb165b7c4a58ebdaa6ebd7
Mayank-Goswami-cg <mayank.goswami.cg@gmail.com>
2026-08-26 11:30:04 +0530

    String operation file updated

--- 05_python_string_operation/demo.py
+++ 05_python_string_operation/demo.py
@@ -17,3 +17,16 @@ print("""Dear Sir,
        Good Morning.""")
 
 print("Dear Ma'am, \n       Good Morning.")
+
+
+#Sting Slicing
+
+print(b[5])    #print(b[starting point : ending point]) 
+print(b[0:5])  #if you want to end in 5 you have to set end point 6 because it gets sliced
+print(b[:6])
+print(b[:])
+
+#Reversing A String
+
+print(b[::1])
+print(b[::-1])
