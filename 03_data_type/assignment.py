4bb73b9cb2edcc6600701b4649107f4701f2a953
Mayank-Goswami-cg <mayank.goswami.cg@gmail.com>
2026-08-26 10:02:56 +0530

    Assignment file updated with all questions

---
+++ 03_data_type/assignment.py
@@ -0,0 +1,70 @@
+# que 24
+student_name="Mayank"
+student_age=18
+student_height=5.4
+is_student=True
+result=None
+
+print(type(student_name))
+print(type(student_age))
+print(type(student_height))
+print(type(is_student))
+print(type(result))
+
+# que 25
+a=50
+b=50.0
+c="50"
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+
+# que 26
+a=True
+b="True"
+
+print(type(a))
+print(type(b))
+
+# que 27
+
+a=None
+b="None"
+
+print(type(a))
+print(type(b))
+
+# The diff between a & b is that the value stored in variable a is None type whereas in b is str type
+
+# que 28
+
+value_int=18
+print(type(value_int))
+value=str(value_int)
+print(type(value))
+
+# que 29
+
+name="Bag"
+quantity=15
+price=1199.00
+available=True
+discount=None
+
+print(type(name))
+print(type(quantity))
+print(type(price))
+print(type(available))
+print(type(discount))
+
+# que 30
+
+a=10
+b=10.0
+c="10"
+d=True
+e="True"
+f=None
+g="None"
