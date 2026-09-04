da464ae5e6aeadf17d6f2ce1f12794dcc9dd0eb8
Mayank-Goswami-cg <mayank.goswami.cg@gmail.com>
2026-09-04 09:35:14 +0530

    assignment of input and output added

---
+++ 07_input_and_output/assignment.py
@@ -0,0 +1,45 @@
+name = input("Enter your Name:")
+print(name)
+
+#Q2
+city = input("Enter your city's name:")
+print(f"Your city is {city}")
+
+#Q3
+your_name=input("Enter your name:")
+age= input("Enter your Age:")
+print(your_name, age)
+
+#Q4
+#What type of value does input() return by default?
+#input() returns the value written inside the parenthisis by default
+
+#Q5
+value=input("Enter your total marks:")
+print(value,type(value))
+
+#Q6
+first_name=input("Enter your first name:")
+last_name=input("Enter your last name:")
+print(first_name,last_name)
+
+#Q7
+namee=input("Enter your FullName")
+cityy=input("Enter your city name:")
+collage=input("Enter your Collage name:")
+print(f"Your name is {namee} and you are from {cityy}, studying in {collage}")
+
+#Q8
+first, second=input("Enter your first and last name").split()
+
+#Q9
+# #Suppose the user enters:
+
+# Python Programming
+# using one input() statement with .split().
+
+# What values will the two variables receive?
+#THE answer will be in list form of ["python", "Programming"]
+
+#Q10
+naam,Age,Gender=input("Enter your name,Age,Gender:")
---
+++ 07_input_and_output/demo.py
@@ -0,0 +1,10 @@
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
+lab=input("Enter your lab name:")
+product_name=input("Enter your product's name:")
+product_price=float(input("Enter your product's price:"))
+count=int(input("Enter products count:"))
+print("Your total is:", product_price * count)
+
+
+
