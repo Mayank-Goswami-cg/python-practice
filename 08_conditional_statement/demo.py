20e8a9ac9c95c29a04e44227b8dc0906f7a5cdda
Mayank-Goswami-cg <mayank.goswami.cg@gmail.com>
2026-09-07 10:58:29 +0530

    demo page updated

--- 08_conditional_statement/demo.py
+++ 08_conditional_statement/demo.py
@@ -19,3 +19,21 @@ if age >= 18:
 
 else :
     print("No seats for minors")
+
+
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+c=input("For addition type 1 or\nFor substraction type 2 or\nFor Multiplication type 3 or\nFor division type 4 or\nFor floor division type 5:")
+
+if c=="1":
+    print(a + b)
+elif c=="2":
+    print(a - b)
+elif c=="3":
+    print(a * b)
+elif c =="4":
+    print(a/b)
+elif c=="5":
+    print(a//b)
+else:
+    print("Invalid value entered")
