6a45979cbad67fa5573f66fded7a0778c921acb0
Mayank-Goswami-cg <mayank.goswami.cg@gmail.com>
2026-09-22 10:05:05 +0530

    6 for loop questions added

---
+++ 00_Problem_Solving_Assignment/For-Loops-Questions.py
@@ -0,0 +1,11 @@
+a=int(input("Enter First number:"))
+b=int(input("Enter Second number:"))
+addition=a+b
+print("Addition is:",addition)
+
+number=int(input("Enter a number:"))
+
+if number%2==0:
+    print("Even")
+else:
+    print(number,"is a Odd number")
